4add98316f47cc4e4f51a807c2deaa4e0ee3c247
Marcio Diaz <marcio.diaz@gmail.com>
2022-09-14 15:36:46 +0100

    Change

--- src/App.js
+++ src/App.js
@@ -44,13 +44,13 @@ export default function App() {
       </div>
       <div className="grid grid-cols-3 text-lg max-w-3xl gap-10 text-blue-200">  
         <div className="w-64">
-          <h3 className="text-pink-300 mb-3">Decentralised</h3> user owns and is sovereign of part of the network.
+          <h3 className="text-pink-300 mb-3">Decentralised</h3> User owns and is sovereign of part of the network.
         </div>
         <div className="w-64">
-          <h3 className="text-pink-300 mb-3">Social enabled</h3> look and feel like Twitter, but everything on-chain.
+          <h3 className="text-pink-300 mb-3">Social</h3> Twitter's look-and-feel and everything on-chain.
         </div>
         <div className="w-64">
-          <h3 className="text-pink-300 mb-3">Crypto powered</h3> token transfers, prediction markets, DAOs, NFTs.
+          <h3 className="text-pink-300 mb-3">Crypto Powered</h3> Token transfers, prediction markets, DAOs, NFTs.
         </div>
       </div>
     </div>
