9099e34942a025373b636b6c6d40fc1922846c47
Marcio Diaz <marcio.diaz@gmail.com>
2022-11-07 10:26:06 +0000

    Add changes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default function App() {
         <img className="opacity-25" src={logo} alt="React Logo" width="300" />
         <img className="opacity-25" src={logo} alt="React Logo" width="300" />
       </div> */}
-      <div className="flex flex-row items-center">
+      <div className="flex flex-column items-center">
           <img src={logo} alt="" className="w-12 h-12 rounded-full mr-5"/>
 
           <h1 className="text-2xl lg:text-5xl text-center">
